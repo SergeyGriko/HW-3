985575aa5e22c6a0b92b9b6ffed759a85527461e
SergeyGriko <206880@gmail.com>
2025-03-30 02:01:26 +0300

    3

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,18 @@ public class Main {
         } else {
             System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
         }
+        int temperature = 13;
+        if (temperature < 5) {
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
+        } else {
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
+        }
+        int speed = 47;
+        if (speed > 60) {
+            System.out.println("Если скорость " + speed + ", то придется заплатить штраф");
+        } else {
+            System.out.println("Если скорость " + speed + ", можно ездить спокойно");
+        }
 
 
 
