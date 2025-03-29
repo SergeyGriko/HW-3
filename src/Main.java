31417ab75e0057abaff227ef90718038413fb82a
SergeyGriko <206880@gmail.com>
2025-03-29 03:24:18 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 50;
+        int age = 43;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
+
 
 
     }
